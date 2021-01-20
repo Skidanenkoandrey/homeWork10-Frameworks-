7e890deae1c1b7aaf65e7e619306bf17a14972a9
Skydanenko Andrii <skidanenkoandrey@gmail.com>
2021-01-20 18:16:13 +0200

    Delated unnecessary font. Added media queries to the navigation and tagged tabs

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -110,10 +110,10 @@ module.exports = {
                     from: `${PATHS.src}/${PATHS.assets}img`,
                     to: `${PATHS.assets}img`
                 },
-                {
-                    from: `${PATHS.src}/${PATHS.assets}fonts`,
-                    to: `${PATHS.assets}fonts`
-                },
+                // {
+                //     from: `${PATHS.src}/${PATHS.assets}fonts`,
+                //     to: `${PATHS.assets}fonts`
+                // },
                 {
                     from: `${PATHS.src}/static`,
                     to: ''
